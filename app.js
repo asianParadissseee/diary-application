d8f46abe0e023bf66d675230e513980f04a78524
asianParadissseee <asian.paradissseee@gmail.com>
2024-12-14 15:22:32 +0500

    feat: swagger docs

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 import express from 'express';
 import cors from 'cors';
+import swaggerUi from 'swagger-ui-express';
 import router from "./routes/index.js";
+import {swaggerDefinition} from "./docs/swaggerDef.js";
 const app = express()
 const port = 3000
+
+const swaggerDocs = {
+    ...swaggerDefinition,
+    paths: {
+        // остальные роуты
+    }
+}
+
 app.use(express.json())
 
 app.use(cors({
@@ -10,11 +20,14 @@ app.use(cors({
     origin: "http://localhost:" + port,
 }))
 app.use("/api", router)
-
+app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 const start = async () => {
     try {
-        app.listen(port, () => console.log(`Example app listening on port ${port}`))
+        app.listen(port, () => {
+            console.log(`App listening on port ${port}`)
+            console.log(`Swagger documentation http://localhost:${port}/swagger`)
+        })
     }catch (e) {
         console.log(e)
         throw new Error(`Произошла ошибка ${e}`);
---
+++ docs/swaggerDef.js
@@ -0,0 +1,14 @@
+export const swaggerDefinition = {
+    openapi: '3.0.0',
+    info: {
+        title: 'My API',
+        version: '1.0.0',
+        description: 'API документация для моего приложения',
+    },
+    servers: [
+        {
+            url: 'http://localhost:3000', // URL вашего сервера
+        },
+    ],
+};
+
