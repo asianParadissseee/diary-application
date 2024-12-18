91f39bbc5c9bec7feb16cd8cf2a42cd09d4abfe1
asianParadissseee <asian.paradissseee@gmail.com>
2024-12-18 13:27:50 +0500

    feat(styles): added tailwind

--- eslint.config.js
+++ eslint.config.js
@@ -4,34 +4,37 @@ import parser from '@typescript-eslint/parser';
 import prettier from 'eslint-plugin-prettier';
 
 export default [
-    {
-        files: ['**/*.{js,jsx,ts,tsx}'],
-        ignores: ['node_modules', 'dist', 'build'],
-        languageOptions: {
-            parser: parser,
-            parserOptions: {
-                ecmaVersion: 2021,
-                sourceType: 'module',
-                ecmaFeatures: {
-                    jsx: true,
-                },
-            },
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    ignores: ['node_modules', 'dist', 'build'],
+    languageOptions: {
+      parser: parser,
+      parserOptions: {
+        ecmaVersion: 2021,
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
         },
-        plugins: {
-            react, // теперь это объект
-            '@typescript-eslint': typescript,
-            prettier, // подключение prettier как плагина
-        },
-        rules: {
-            'prettier/prettier': 'error',
-            'react/react-in-jsx-scope': 'off', // Не нужно для новых React-проектов
-            '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
-            '@typescript-eslint/explicit-module-boundary-types': 'off',
-        },
-        settings: {
-            react: {
-                version: 'detect', // Автоматическое определение версии React
-            },
-        },
+      },
     },
+    plugins: {
+      react, // теперь это объект
+      '@typescript-eslint': typescript,
+      prettier, // подключение prettier как плагина
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      'react/react-in-jsx-scope': 'off', // Не нужно для новых React-проектов
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        { argsIgnorePattern: '^_' },
+      ],
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+    },
+    settings: {
+      react: {
+        version: 'detect', // Автоматическое определение версии React
+      },
+    },
+  },
 ];
---
+++ src/app/app.tsx
@@ -0,0 +1,5 @@
+const App = () => {
+  return <div>work</div>;
+};
+
+export default App;
